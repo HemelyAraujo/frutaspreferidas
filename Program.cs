7366e2d1249a1c8358bb32cdd6a0a7ef35c1faf0
HemelyAraujo <hemelyraujo21@gmail.com>
2023-03-10 22:10:21 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("frutaspreferidas");
+Console.WriteLine("1- Morango");
+Console.WriteLine("2- Kiwi");
+Console.WriteLine("3- Lichia");
+Console.WriteLine("4- Pitaya");
+Console.WriteLine("5- Melão");
